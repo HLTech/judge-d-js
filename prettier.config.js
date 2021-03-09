48d5586479e53209af9823142bacea5a488e6299
Pawel <pawel.fajfer@hl.co.uk>
2021-03-09 10:37:04 +0100

    Create new node.js.yml

--- prettier.config.js
+++ prettier.config.js
@@ -2,4 +2,12 @@ module.exports = {
     singleQuote: true,
     tabWidth: 4,
     trailingComma: 'es5',
+    overrides: [
+        {
+            files: ['.github/**/*.yml'],
+            options: {
+                tabWidth: 2,
+            },
+        },
+    ],
 };
