f02264c0d2cbe605581a8cb034c9bcd56ae87dda
Pawfa <fajfer.pawel@gmail.com>
2021-03-18 11:39:27 +0100

    fix: Improved url parsing

--- src/utils/build-url.ts
+++ src/utils/build-url.ts
@@ -1,7 +1,12 @@
+import { URL } from 'url';
+
 export function buildUrl(
     url: string,
     serviceName: string,
     serviceVersion: string
 ) {
-    return `${url}/contracts/services/${serviceName}/versions/${serviceVersion}`;
+    return new URL(
+        `./contracts/services/${serviceName}/versions/${serviceVersion}`,
+        url
+    ).toString();
 }
