88aa05c38626e241afe972f3fc2a25e8f12cfea7
Pawfa <fajfer.pawel@gmail.com>
2022-02-23 16:13:20 +0100

    refactor: Rename service contracts form post function to handle capabilities

--- src/api/post-service-contracts-form.ts
+++ src/api/post-service-contracts-form.ts
@@ -1,6 +1,9 @@
 import { ServiceContractsForm } from '../utils/generate-contracts-form';
 import axios from 'axios';
 
-export function postPacts(url: string, pacts: ServiceContractsForm) {
+export function postServiceContractsForm(
+    url: string,
+    pacts: ServiceContractsForm
+) {
     return axios.post(url, pacts);
 }
